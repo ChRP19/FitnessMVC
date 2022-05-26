63943eab0edad5f813ed610f73b7815a83ad4923
Roman Chirkov <r.tchirkov2010@yandex.ru>
2022-05-27 00:56:25 +0300

    Create User model

---
+++ FitnessMVC.BL/Model/Gender.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FitnessMVC.BL.Model
+{
+	public class Gender
+	{
+		public Gender(string name)
+		{
+			if(string.IsNullOrWhiteSpace(name))
+			{
+				throw new ArgumentNullException("Gender cannot be empty", nameof(name));
+			}
+			Name = name;
+		}
+		public string Name { get; }
+
+		public override string ToString()
+		{
+			return Name;
+		}
+	}
+}
---
+++ FitnessMVC.BL/Model/User.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection.Metadata;
+
+namespace FitnessMVC.BL.Model
+{
+	public class User
+	{
+		public User(string name, Gender gender, DateTime birthday, double weight, double height)
+		{
+			#region Verification
+			if(string.IsNullOrWhiteSpace(name))
+			{
+				throw new ArgumentNullException("Name can not be empty.", nameof(name));
+			}
+			if(gender == null)
+			{
+				throw new ArgumentNullException("Gender cannot be null.", nameof(gender));
+			}
+			if(birthday < DateTime.Parse("01.01.1900") || birthday >= DateTime.Now)
+			{
+				throw new ArgumentException("Impossible date of birth." ,nameof(birthday));
+			}
+			if(weight <= 0)
+			{
+				throw new ArgumentException("Wrong weight.", nameof(weight));
+			}
+			if(height <= 0)
+			{
+				throw new ArgumentException("Wrong height.", nameof(height));
+			}
+			#endregion
+			
+			Name = name;
+			Gender = gender;
+			Birthday = birthday;
+			Weight = weight;
+			Height = height;
+		}
+		public string Name { get; }
+
+		public Gender Gender { get; }
+
+		public DateTime Birthday { get; }
+
+		public double Weight { get; set; }
+
+		public double Height { get; set; }
+
+		public override string ToString()
+		{
+			return Name;
+		}
+	}
+
+}
